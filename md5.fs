53694df6958d1b06d3b2d86c3eb06b5c0f29b682
Weldon Goree <weldon@b.rontosaur.us>
2012-04-16 19:44:22 -0400

    pad length

--- md5.fs
+++ md5.fs
@@ -10,3 +10,21 @@ hex
     2over 20 - rshift -rot
     lshift +
 ;
+
+: not ( x -- x )
+    -1 xor
+;
+
+: find-pad-length
+    dup 200 mod 1C0 -
+    dup 0 <= if
+	dup 200 +
+    then
+;
+
+\ NB: pad-message incorrectly assumes the message is byte-aligned at its end. This is a bug.
+
+: pad-message ( x-addr n -- x-addr n )
+    find-pad-length over +
+    
+;
